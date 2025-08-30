b8ecfbfcc5cb90edb63215697649cf9e259dc235
Agradeep1920G <agradeep7654@gmail.com>
2025-08-30 23:35:44 +0530

    Removed OntologyGraph component and references

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import { SkillsSection } from './components/SkillsSection';
 import { ProjectsSection } from './components/ProjectsSection';
 import { WebCapabilitiesSection } from './components/WebCapabilitiesSection';
 import { ContactSection } from './components/ContactSection';
-import OntologyGraph from './components/OntologyGraph';
+
 
 export default function WebDeveloperPortfolio() {
 	return (
 		<main className="min-h-screen bg-[#0D1117] text-white">
 			<HeroSection />
-			<OntologyGraph />
+
 			<SkillsSection />
 			<ProjectsSection />
 			<WebCapabilitiesSection />
